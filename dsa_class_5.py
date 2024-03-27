81a0e4ed7cb26513ca9ac69c9c3723f61b7b7483
Rifat <rifatyou2686@gmail.com>
2024-03-28 02:38:54 +0600

    2 problem solve truncateSentence and  arrayStringsAreEqual

--- dsa_class_5.py
+++ dsa_class_5.py
@@ -1,5 +1,6 @@
-##> r
-'''
+##> w r
+dsa_classs_5 = print
+"""
 771. Jewels and Stones
 Easy
 Topics
@@ -26,13 +27,119 @@ Constraints:
 1 <= jewels.length, stones.length <= 50
 jewels and stones consist of only English letters.
 All the characters of jewels are unique.
-'''
+"""
+
 
 def numJewelsInStones(jewels, stones):
-    cnt=0;
+    cnt = 0
     for ch in stones:
         if ch in jewels:
-            cnt+=1;
-    return cnt;
-count=numJewelsInStones(jewels = "z", stones = "ZZ");
-print(count);
+            cnt += 1
+    return cnt
+
+
+count = numJewelsInStones(jewels="z", stones="ZZ")
+print(count)
+
+"""
+1816. Truncate Sentence
+Easy
+Topics
+Companies
+Hint
+A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+ 
+
+Example 1:
+
+Input: s = "Hello how are you Contestant", k = 4
+Output: "Hello how are you"
+Explanation:
+The words in s are ["Hello", "how" "are", "you", "Contestant"].
+The first 4 words are ["Hello", "how", "are", "you"].
+Hence, you should return "Hello how are you".
+Example 2:
+
+Input: s = "What is the solution to this problem", k = 4
+Output: "What is the solution"
+Explanation:
+The words in s are ["What", "is" "the", "solution", "to", "this", "problem"].
+The first 4 words are ["What", "is", "the", "solution"].
+Hence, you should return "What is the solution".
+Example 3:
+
+Input: s = "chopper is not a tanuki", k = 5
+Output: "chopper is not a tanuki"
+ 
+
+Constraints:
+
+1 <= s.length <= 500
+k is in the range [1, the number of words in s].
+s consist of only lowercase and uppercase English letters and spaces.
+The words in s are separated by a single space.
+There are no leading or trailing spaces.
+"""
+
+
+def truncateSentence(s, k):
+    split_s = s.split()
+    return " ".join(split_s[0:k])
+
+
+dsa_classs_5(truncateSentence("Hello how are you Contestant", k=2))
+
+
+"""
+1662. Check If Two String Arrays are Equivalent
+Easy
+Topics
+Companies
+Hint
+Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+A string is represented by an array if the array elements concatenated in order forms the string.
+
+ 
+
+Example 1:
+
+Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+Output: true
+Explanation:
+word1 represents string "ab" + "c" -> "abc"
+word2 represents string "a" + "bc" -> "abc"
+The strings are the same, so return true.
+Example 2:
+
+Input: word1 = ["a", "cb"], word2 = ["ab", "c"]
+Output: false
+Example 3:
+
+Input: word1  = ["abc", "d", "defg"], word2 = ["abcddefg"]
+Output: true
+ 
+
+Constraints:
+
+1 <= word1.length, word2.length <= 103
+1 <= word1[i].length, word2[i].length <= 103
+1 <= sum(word1[i].length), sum(word2[i].length) <= 103
+word1[i] and word2[i] consist of lowercase letters.
+"""
+
+
+def arrayStringsAreEqual(word1, word2):
+    join_word1 = "".join(word1)
+    join_word2 = "".join(word2)
+
+    if join_word1 == join_word2:return True;
+    else:return False;
+    
+Equvalent=arrayStringsAreEqual( ["ab", "c"], word2 = ["a", "bc"])
+dsa_classs_5(Equvalent);
+        
